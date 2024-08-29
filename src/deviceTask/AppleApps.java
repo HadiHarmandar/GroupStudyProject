f76c14368ba9fb92c29b9bb7e5b8045f7ea7adf7
HadiHarmandar <hadiharmandar1@gmail.com>
2024-08-28 20:15:51 -0500

    Interfaces created

---
+++ src/deviceTask/AppleApps.java
@@ -0,0 +1,14 @@
+package deviceTask;
+
+public interface AppleApps extends Downloadable {
+
+    String APP_STORE_NAME = "Apple Store";
+    String OS = "IOS";
+
+}
+/*
+6. Create a Child Interface of Downloadable Named 'AppleApps':
+        - Variables:
+        - AppStoreName
+        - OS
+ */
---
+++ src/deviceTask/Downloadable.java
@@ -0,0 +1,12 @@
+package deviceTask;
+
+public interface Downloadable {
+
+    void downloadApp();
+
+}
+/*
+4. Create an Interface Named 'Downloadable':
+    - Abstract Method:
+        - downloadApp()
+ */
